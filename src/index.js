0efc8347bc44d21d92dc23e79a912004ed117bd7
YingWeiPeng94 <1727142928@qq.com>
2020-04-07 20:15:38 +0800

    123

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom';
 import "./index.css"
 import CheckBoxGroup from "./components/common/CheckBoxGroup/Test"
 import RadioBoxGroup from "./components/common/RadioBoxGroup/Test"
-import Select from "./components/common/Select/Test"
+// import Select from "./components/common/Select/Test"
 ReactDOM.render(<div>
     <CheckBoxGroup></CheckBoxGroup>
     <hr/>
     <RadioBoxGroup></RadioBoxGroup>
     <hr/>
-    <Select>
+   {/*  <Select>
         
-    </Select>
+    </Select> */}
     </div>, document.getElementById('root'));

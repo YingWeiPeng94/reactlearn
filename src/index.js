de9797e4131809a5153e3c2e918682ef2544b107
YingWeiPeng94 <1727142928@qq.com>
2020-06-07 21:09:03 +0800

    2020-6-7-21

--- src/index.js
+++ src/index.js
@@ -1,19 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import "./index.css";
-<<<<<<< HEAD
-
-const div = (
-    <div>
-
-=======
 import App3 from './App3'
 import App4 from './App4'
 const div = (
     <div>
        {/* <App3></App3> */}
        <App4></App4>
->>>>>>> 3-10PureComponent
     </div>
 );
 const div2 = <div className="assaf">
